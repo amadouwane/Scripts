8db8dae7b3862cdcfdc30cc41e8d6aba73b800a5
Amadou Wane <amadou@amadouwane.com>
2014-09-04 15:50:31 -0400

    Create CDR_Handler_Restart.sh

---
+++ Oi/CDR_Handler_Restart.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#mv /app/dre/thirdparty/jboss-5.1.0.GA/server/default/deploy/CDRHandler.jar /app/dre/thirdparty/jboss-5.1.0.GA/server/default/deploy/CDRHandler.jar.original
+sleep 30
+mv /app/dre/thirdparty/jboss-5.1.0.GA/server/default/deploy/CDRHandler.jar.original /app/dre/thirdparty/jboss-5.1.0.GA/server/default/deploy/CDRHandler.jar
+echo "Done"
